76dc6b8fe3e3b708f7f3ea10c28cda2a0dc56e0a
Cory Larcade <clarcade@gmail.com>
2024-11-12 22:31:13 -0800

    feat: updating home page by add Resume Summary section and Skill set section, adding ResumeSummaryCard/SideBySideCard/SkillsCard components, and image of myself

---
+++ src/data/skills-data.ts
@@ -0,0 +1,74 @@
+import type {SkillsData} from "../components/SkillsCard.astro";
+
+export const frontendSkillsData: Array<SkillsData> = [
+	{
+		topic: 'HTML 5',
+		items: ['SEO'],
+	},
+	{
+		topic: 'CSS',
+		items: [
+			'SCSS, LESS',
+			'BEM',
+			'Responsive design, Mobile-first',
+		],
+	},
+	{
+		topic: 'Javascript',
+		items: [
+			'ES6, Typescript',
+			'React, Gatsby, AngularJS, Polymer',
+			'Jest, Enzyme, Mocha, Chai, React Testing Library',
+		],
+	},
+];
+
+export const backendSkillsData: Array<SkillsData> = [
+	{
+		topic: 'Node',
+		items: ['Express'],
+	},
+	{
+		topic: 'PHP',
+	},
+	{
+		topic: 'AWS (breadth, not depth)',
+	},
+	{
+		topic: 'Jenkins',
+	},
+	{
+		topic: 'CMS and Databases',
+		items: [
+			'Contentful',
+			'MongoDB, Firebase',
+			'MySQL, SQL',
+		],
+	},
+];
+
+export const otherSkillsData: Array<SkillsData> = [
+	{
+		topic: 'Design',
+		items: [
+			'Figma',
+			'Sketch',
+		],
+	},
+	{
+		topic: 'Devops',
+		items: [
+			'Webpack',
+			'Jarvis',
+		],
+	},
+	{
+		topic: 'Other tools',
+		items: [
+			'New Relic',
+			'Selenium',
+			'Github, JIRA',
+			'Netlify, Heroku',
+		],
+	},
+];
