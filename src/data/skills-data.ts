5e2d92d36d07e5b89741356eb0419db04bd88ade
Cory Larcade <clarcade@gmail.com>
2024-11-22 22:27:42 -0800

    feat: moving all skills data to skills-data.ts, updating home page accordingly, added new homepage picture of myself wearing a hat, updating picture sizes, and updating styling for SideBySideCard and SkillsCard

--- src/data/skills-data.ts
+++ src/data/skills-data.ts
@@ -1,74 +1,95 @@
-import type {SkillsData} from '../components/SkillsCard.astro';
+import type {SkillsCardData} from '../components/SkillsCard.astro';
 
-export const frontendSkillsData: Array<SkillsData> = [
-	{
-		topic: 'HTML 5',
-		items: ['SEO'],
-	},
-	{
-		topic: 'CSS',
-		items: [
-			'SCSS, LESS',
-			'BEM',
-			'Responsive design, Mobile-first',
-		],
-	},
-	{
-		topic: 'Javascript',
-		items: [
-			'ES6, Typescript',
-			'React, Gatsby, AngularJS, Polymer',
-			'Jest, Enzyme, Mocha, Chai, React Testing Library',
-		],
-	},
-];
+export const frontendSkillsData: SkillsCardData = {
+	img: 'https://placehold.co/300x400',
+	imgAltText: 'Frontend skills image',
+	title: 'Frontend Skills',
+	items: [
+		{
+			topic: 'HTML 5',
+			items: ['SEO'],
+		},
+		{
+			topic: 'CSS',
+			items: [
+				'SCSS, LESS',
+				'BEM',
+				'Responsive design, Mobile-first',
+			],
+		},
+		{
+			topic: 'Javascript',
+			items: [
+				'ES6, Typescript',
+				'React, Gatsby, AngularJS, Polymer',
+				'Jest, Enzyme, Mocha, Chai, React Testing Library',
+			],
+		},
+	],
+};
 
-export const backendSkillsData: Array<SkillsData> = [
-	{
-		topic: 'Node',
-		items: ['Express'],
-	},
-	{
-		topic: 'PHP',
-	},
-	{
-		topic: 'AWS (breadth, not depth)',
-	},
-	{
-		topic: 'Jenkins',
-	},
-	{
-		topic: 'CMS and Databases',
-		items: [
-			'Contentful',
-			'MongoDB, Firebase',
-			'MySQL, SQL',
-		],
-	},
-];
+export const backendSkillsData: SkillsCardData = {
+	img: 'https://placehold.co/300x400',
+	imgAltText: 'Backend skills image',
+	title: 'Backend Skills',
+	items: [
+		{
+			topic: 'Node',
+			items: ['Express'],
+		},
+		{
+			topic: 'PHP',
+		},
+		{
+			topic: 'AWS (breadth, not depth)',
+		},
+		{
+			topic: 'Jenkins',
+		},
+		{
+			topic: 'CMS and Databases',
+			items: [
+				'Contentful',
+				'MongoDB, Firebase',
+				'MySQL, SQL',
+			],
+		},
+	],
+};
 
-export const otherSkillsData: Array<SkillsData> = [
-	{
-		topic: 'Design',
-		items: [
-			'Figma',
-			'Sketch',
-		],
-	},
-	{
-		topic: 'Devops',
-		items: [
-			'Webpack',
-			'Jarvis',
-		],
-	},
-	{
-		topic: 'Other tools',
-		items: [
-			'New Relic',
-			'Selenium',
-			'Github, JIRA',
-			'Netlify, Heroku',
-		],
-	},
+export const otherSkillsData: SkillsCardData = {
+	img: 'https://placehold.co/300x400',
+	imgAltText: 'Other skills image',
+	title: 'Other Skills',
+	items: [
+		{
+			topic: 'Design',
+			items: [
+				'Figma',
+				'Sketch',
+			],
+		},
+		{
+			topic: 'Devops',
+			items: [
+				'Webpack',
+				'Jarvis',
+			],
+		},
+		{
+			topic: 'Other tools',
+			items: [
+				'New Relic',
+				'Selenium',
+				'Github, JIRA',
+				'Netlify, Heroku',
+			],
+		},
+	],
+};
+
+export const skillsData: Array<SkillsCardData> = [
+	frontendSkillsData,
+	backendSkillsData,
+	otherSkillsData,
 ];
